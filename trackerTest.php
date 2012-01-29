32345fe231a2a16ae0c2b8aae3c16218a51384f4
Buzztea <buzztea@gmail.com>
2012-01-28 21:26:05 -0800

    Checking in tracker Test Page

---
+++ trackerTest.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>TestPage</title>
+<script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script>
+<script type="text/javascript">
+       tracker = _gat._createTracker('UA-28705338-1');
+       tracker._setDomainName('http://www.chirped.it/');
+       tracker._trackEvent( 'Home', 'Hit site');
+//       _gaq.push(['_setAccount', 'UA-28705338-1']);
+//       _gaq.push(['_trackPageview']);
+//       _gaq.push(['_setDomainName', 'http://www.chirped.it/']);
+//       _gaq.push(['_trackEvent', 'Home', 'Hit site']);
+</script>
+</head>
+</html>
